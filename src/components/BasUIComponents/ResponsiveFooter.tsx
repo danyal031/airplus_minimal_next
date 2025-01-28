d0b721a2c48c5e15d3474bcd9270e7096013bcc1
danial <danialhooshangi41@gmail.com>
2025-01-28 16:51:38 +0330

    `Refactor ResponsiveFooter component to improve layout and add collapsible sections`

--- src/components/BasUIComponents/ResponsiveFooter.tsx
+++ src/components/BasUIComponents/ResponsiveFooter.tsx
@@ -2,6 +2,7 @@
 import {
   BottomNavigation,
   Box,
+  Collapse,
   Divider,
   IconButton,
   TextField,
@@ -9,7 +10,7 @@ import {
 } from "@mui/material";
 import Image from "next/image";
 import Link from "next/link";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import VerifiedIcon from "@mui/icons-material/Verified";
 import TelegramIcon from "@mui/icons-material/Telegram";
 import InstagramIcon from "@mui/icons-material/Instagram";
@@ -21,12 +22,16 @@ import TaskAltIcon from "@mui/icons-material/TaskAlt";
 import warrantyLottie from "../../../public/assets/lottie/warranty.json";
 import Lottie from "lottie-react";
 import { ConfigDataType } from "@/DataTypes/globalTypes";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 const ResponsiveFooter = () => {
   // initial states
   const [config, setConfig] = React.useState<null | null | ConfigDataType>(
     null
   );
   const [value, setValue] = React.useState(0);
+  const [openTabId, setOpenTabId] = useState<null | string>(null);
+
   const theme = useTheme();
 
   // handle initial value
@@ -354,108 +359,150 @@ const ResponsiveFooter = () => {
     return (
       <>
         {" "}
-        <Box sx={{ width: "100%" }} className="md:hidden">
-          <BottomNavigation
-            showLabels
-            value={value}
-            onChange={(event, newValue) => {
-              setValue(newValue);
-            }}
-          >
-            <div
-              className="w-full grid grid-cols-1 md:grid-cols-3 px-2 md:px-52"
-              style={{
-                backgroundImage: `url('/assets/images/footerBanner/footer-poster.png')`,
-                backgroundRepeat: "no-repeat",
-                overflow: "hidden",
-              }}
-            >
-              <div className="flex flex-col justify-center items-start gap-5">
-                <div className="flex items-center justify-center gap-2">
-                  <LocationOnIcon fontSize="small" />
-                  <span className="text-sm">
-                    ایران، اصفهان، بلوار کشاورز، ابتدای کنارگذر شهید میثمی،
-                    تعاونی مصرف کارکنان ذوب آهن اصفهان، آژانس مهروماه آسمان
-                  </span>
-                </div>
-                <div className="flex items-center justify-center gap-2">
-                  <TtyIcon fontSize="small" />
-                  <span className="text-sm">
-                    اصفهان{" "}
-                    <a href="tel:03132110" dir="ltr">
-                      031-32110
-                    </a>
-                  </span>
-                  <TtyIcon fontSize="small" />
-                  <span className="text-sm">
-                    تهران{" "}
-                    <a href="tel:02191016838" dir="ltr">
-                      021-91016838
-                    </a>
-                  </span>
-                </div>
-                <div className="flex items-center justify-center gap-2">
-                  <AlternateEmailIcon fontSize="small" />
-                  <span className="text-sm">
-                    <a href="mail:info@mehromah24.com" dir="ltr">
-                      info [at] mehromah24 [dot] com
-                    </a>
-                  </span>
-                </div>
-              </div>
-              <div className="flex-col justify-center items-start text-xs gap-5 px-10 hidden md:flex">
-                <div className="flex gap-1 items-center">
-                  <AttachmentIcon />
-                  <Link
-                    href="https://farasa.cao.ir/sysworkflow/fa/modern/3810212626028ab03488017019616799/6464336316028ab04e3c618028352200.php"
-                    target="_blank"
-                  >
-                    حقوق مسافران
-                  </Link>
-                </div>
-                <div className="flex gap-1 items-center">
-                  <AttachmentIcon />{" "}
-                  <Link href="http://caa.gov.ir/" target="_blank">
-                    سازمان هواپیمایی کشور
-                  </Link>
-                </div>
-                <div className="flex gap-1 items-center">
-                  <AttachmentIcon />{" "}
-                  <Link
-                    href="https://logo.samandehi.ir/Verify.aspx?id=301458&p=xlaoobpdrfthaodsdshwmcsi"
-                    target="_blank"
-                  >
-                    نماد سازماندهی
-                  </Link>
-                </div>
-                <div className="flex gap-1 items-center">
-                  <AttachmentIcon />{" "}
-                  <Link
-                    href="https://mehromah24.ir/api/DeepLink/Refund/V1?language=fa"
-                    target="_blank"
-                  >
-                    سامانه استردادی
-                  </Link>
-                </div>
-              </div>
-              <div className="justify-center items-center hidden md:flex">
-                <span>
-                  <div>
-                    <Link href="https://enamad.ir" target="_blank">
-                      {/* <Image
-                        src={enamad}
-                        alt="Enamad Logo"
-                        width={100}
-                        height={100}
-                      /> */}
-                      enamad
-                    </Link>
-                  </div>
+        <div className="relative md:hidden flex flex-col items-stretch justify-between min-w-full min-h-96 border-t-2 border-paper bg-main">
+          {/*  */}
+          <div className="grid grid-cols-1 gap-4 p-5 pb-2">
+            <span className="text-2xl text-text-main font-bold flex items-center justify-start pr-7">
+              Airplus
+            </span>
+            <div className="grid grid-cols-1 gap-3">
+              <span className="text-sm font-bold text-text-main text-justify leading-7">
+                آدرس: {config?.communicational.address.fa}
+              </span>
+              <div className="flex items-center justify-start gap-1">
+                <span className="text-sm font-bold text-text-main">
+                  تلفن پشتیبانی:
                 </span>
+                <a
+                  href={`tel:${config?.communicational.phone}`}
+                  className="text-sm font-bold text-text-main"
+                >
+                  {config?.communicational.phone.slice(0, 3) +
+                    "-" +
+                    config?.communicational.phone.slice(3)}
+                </a>
+              </div>
+              <div className="flex items-center justify-start gap-1 text-text-main">
+                <span className="text-sm font-bold">ایمیل:</span>
+                <a
+                  href={`mailto:${config?.communicational.email}`}
+                  className="text-sm font-bold"
+                >
+                  {config?.communicational.email}
+                </a>
+              </div>
+              <div className="py-1 grid grid-cols-1 gap-1">
+                <div className="flex items-center justify-between text-text-main font-bold text-sm">
+                  <span>خدمات مشتریان</span>
+                  <IconButton
+                    size="small"
+                    onClick={() => {
+                      setOpenTabId(
+                        openTabId === "customerServices"
+                          ? null
+                          : "customerServices"
+                      );
+                    }}
+                  >
+                    {openTabId === "customerServices" ? (
+                      <KeyboardArrowUpIcon fontSize="small" />
+                    ) : (
+                      <KeyboardArrowDownIcon fontSize="small" />
+                    )}
+                  </IconButton>
+                </div>{" "}
+                <Collapse
+                  in={openTabId === "customerServices"}
+                  timeout="auto"
+                  // unmountOnExit
+                >
+                  <div className="flex flex-col items-start justify-center gap-2 p-2">
+                    {customerServicesOptions.map((item) => {
+                      return (
+                        <>
+                          <Link
+                            href={item.link}
+                            key={item.id}
+                            className="text-sm text-text-main font-semibold hover:text-primary-main transition-colors duration-300"
+                          >
+                            {item.label}
+                          </Link>
+                        </>
+                      );
+                    })}
+                  </div>
+                </Collapse>
+              </div>
+              <div className="py-1 grid grid-cols-1 gap-1">
+                <div className="flex items-center justify-between text-text-main font-bold text-sm">
+                  <span>اطلاعات تکمیلی</span>
+                  <IconButton
+                    size="small"
+                    onClick={() => {
+                      setOpenTabId(
+                        openTabId === "complementaryInfoOptions"
+                          ? null
+                          : "complementaryInfoOptions"
+                      );
+                    }}
+                  >
+                    {openTabId === "complementaryInfoOptions" ? (
+                      <KeyboardArrowUpIcon fontSize="small" />
+                    ) : (
+                      <KeyboardArrowDownIcon fontSize="small" />
+                    )}{" "}
+                  </IconButton>
+                </div>
+                <Collapse
+                  in={openTabId === "complementaryInfoOptions"}
+                  timeout="auto"
+                  // unmountOnExit
+                >
+                  <div className="flex flex-col items-start justify-center gap-2 p-2">
+                    {complementaryInfoOptions.map((item) => {
+                      return (
+                        <>
+                          <Link
+                            href={item.link}
+                            key={item.id}
+                            className="text-sm text-text-main font-semibold hover:text-primary-main transition-colors duration-300"
+                          >
+                            {item.label}
+                          </Link>
+                        </>
+                      );
+                    })}
+                  </div>
+                </Collapse>
+              </div>
+              <div className="flex flex-col items-center justify-center gap-1">
+                <span className="text-text-main text-sm font-bold">
+                  به خانواده بزرگ ما بپیوندید
+                </span>
+                <div className="flex items-center justify-center gap-2">
+                  <TelegramIcon
+                    fontSize="medium"
+                    className="hover:cursor-pointer text-primary-main transition-colors duration-300"
+                  />
+                  <InstagramIcon
+                    fontSize="medium"
+                    className="hover:cursor-pointer text-primary-main transition-colors duration-300"
+                  />
+                </div>
               </div>
             </div>
-          </BottomNavigation>
-        </Box>
+          </div>
+          <div className="flex items-center justify-center w-full bg-primary-main p-2 text-paper">
+            <p className="text-xs font-light">
+              سوپر اپلیکیشن {config?.title.fa} قدرت گرفته از [{" "}
+              <VerifiedIcon fontSize="small" className="text-xs" />{" "}
+              <Link href={"https://airplus.app"} target="_blank">
+                ایرپلاس
+              </Link>{" "}
+              ] - © 1403
+            </p>
+          </div>
+        </div>
       </>
     );
   };
