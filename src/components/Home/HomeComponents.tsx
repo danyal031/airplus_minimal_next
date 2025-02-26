758870a49351a35d78f18531cbe66dc7e039baad
danial <danialhooshangi41@gmail.com>
2025-02-26 15:28:53 +0330

    `Refactor HomeComponents and App components with minor changes`

--- src/components/Home/HomeComponents.tsx
+++ src/components/Home/HomeComponents.tsx
@@ -4,17 +4,28 @@ import Services from "./indexPageComponents/Services";
 import TabDescriptionsComponent from "./indexPageComponents/TabDescriptionsComponent";
 import { getAirportsInServer } from "@/global-files/fetches";
 import { AirportDataType } from "@/DataTypes/flight/flightTicket";
-
+import HeadsetMicIcon from "@mui/icons-material/HeadsetMic";
 const HomeComponents = async () => {
   const airports: AirportDataType[] = await getAirportsInServer();
 
   const renderOnDesktop = () => {
     return (
       <>
-        <div className="container hidden md:grid grid-cols-1 gap-14 py-24">
-          <SearchBox airports={airports} />
-          <Services />
-          <TabDescriptionsComponent />
+        <div className="container hidden md:block py-24">
+          <div className="relative grid grid-cols-1 gap-14">
+            {/* <div className="z-10 sticky top-[760px] -ml-24 flex flex-col items-center justify-center gap-2 justify-self-end">
+              <span className="bg-paper h-12 w-12 rounded-full shadow-md shadow-primary-main flex items-center justify-center">
+                H
+              </span>
+              <span className="bg-primary-main h-12 w-12 rounded-full flex items-center justify-center">
+                <HeadsetMicIcon fontSize="small" className="text-paper" />
+              </span>
+            </div> */}
+
+            <SearchBox airports={airports} />
+            <Services />
+            <TabDescriptionsComponent />
+          </div>
         </div>
       </>
     );
