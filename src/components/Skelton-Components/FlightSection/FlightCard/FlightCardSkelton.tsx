3b18117c67a6d3905200e1de62d1b4d3ee550407
danial <danialhooshangi41@gmail.com>
2025-02-26 10:48:15 +0330

    `Refactor FlightCardSkelton component layout and styles`

--- src/components/Skelton-Components/FlightSection/FlightCard/FlightCardSkelton.tsx
+++ src/components/Skelton-Components/FlightSection/FlightCard/FlightCardSkelton.tsx
@@ -4,28 +4,59 @@ export const FlightCardSkelton = () => {
   const renderSkeltonOnDesktop = () => {
     return (
       <>
-        {" "}
-        <div className="w-full h-[100px] hidden md:grid grid-cols-12 gap-5 bg-white border border-neutral-300 rounded-2xl px-6">
-          <div className="col-span-1 flex justify-center items-center">
-            <Skeleton variant="circular" width={"40px"} height={"40px"} />
+        <div className="bg-paper border border-divider rounded-2xl w-full hidden md:grid grid-cols-12 gap-5 p-4">
+          <div className="col-span-2 flex flex-col items-center justify-center gap-2">
+            <Skeleton
+              variant="circular"
+              width={"40px"}
+              height={"40px"}
+              animation="wave"
+            />
+            <Skeleton
+              variant="text"
+              width={"80%"}
+              height={"20px"}
+              animation="wave"
+            />
           </div>
-          <div className="col-span-2 flex justify-center items-center">
-            <Skeleton variant="rounded" width={"100%"} height={"30px"} />
+          <div className="col-span-7 flex flex-col items-start justify-between">
+            <Skeleton
+              variant="text"
+              width={"100%"}
+              height={"30px"}
+              animation="wave"
+            />
+            <div className="flex items-center justify-start gap-3">
+              <Skeleton
+                variant="rounded"
+                width={"80px"}
+                height={"22px"}
+                className="rounded-xl"
+                animation="wave"
+              />{" "}
+              <Skeleton
+                variant="rounded"
+                width={"80px"}
+                height={"22px"}
+                className="rounded-xl"
+                animation="wave"
+              />
+            </div>
           </div>
-          <div className="col-span-2 flex flex-col justify-center items-center">
-            <Skeleton variant="text" width={"100%"} height={"30px"} />
-            <Skeleton variant="text" width={"100%"} height={"30px"} />
-          </div>
-          <div className="col-span-2 flex flex-col justify-center items-center">
-            <Skeleton variant="text" width={"100%"} height={"30px"} />
-            <Skeleton variant="text" width={"100%"} height={"30px"} />
-          </div>
-          <div className="col-span- flex flex-col justify-center items-center">
-            <Skeleton variant="text" width={"100%"} height={"30px"} />
-            <Skeleton variant="text" width={"100%"} height={"30px"} />
-          </div>
-          <div className="col-span-2 flex justify-center items-center">
-            <Skeleton variant="rounded" width={"100%"} height={"30px"} />
+          <div className="col-span-3 flex flex-col items-center justify-center gap-3">
+            <Skeleton
+              variant="rounded"
+              width={"130px"}
+              height={"25px"}
+              className="rounded-xl"
+              animation="wave"
+            />
+            <Skeleton
+              variant="rounded"
+              width={"80px"}
+              height={"18px"}
+              animation="wave"
+            />
           </div>
         </div>
       </>
