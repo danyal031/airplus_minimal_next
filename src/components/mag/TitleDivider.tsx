c9af5b1bd2c870763728d55c99f651586bae9076
danial <danialhooshangi41@gmail.com>
2025-03-10 16:39:08 +0330

    `feat: updated ArticleList component with new functionality and UI`

---
+++ src/components/mag/TitleDivider.tsx
@@ -0,0 +1,17 @@
+import React, { FC } from "react";
+
+interface TitleDividerProps {
+  label: string;
+  icon?: any;
+}
+const TitleDivider: FC<TitleDividerProps> = ({ label, icon }) => {
+  return (
+    <div className="col-span-12 flex items-center justify-center gap-2">
+      {icon && icon}
+      <span className="text-base font-semibold text-text-main">{label}</span>
+      <div className="relative grow h-px bg-divider rounded-full overflow-hidden"></div>
+    </div>
+  );
+};
+
+export default TitleDivider;
--- src/components/mag/magIndexPage/ArticleList.tsx
+++ src/components/mag/magIndexPage/ArticleList.tsx
@@ -1,9 +1,34 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
+import TitleDivider from "../TitleDivider";
+import { ArticleDataTypes } from "@/DataTypes/mag/articleListTypes";
 
 const ArticleList = () => {
+  // initial states
+  const [latestArticles, setLatestArticles] = useState<ArticleDataTypes[]>([]);
+  const [mostViewedArticles, setMostViewedArticles] = useState<
+    ArticleDataTypes[]
+  >([]);
+  const [showLatestLoading, setShowLatestLoading] = useState<boolean>(true);
+  const [showMostViewedLoading, setShowMostViewedLoading] =
+    useState<boolean>(true);
+
+  //   handle get latest articles & most viewed articles
+  useEffect(() => {
+    // get latest articles
+    // get most viewed articles
+  }, []);
+
   // for render most viewed
   const renderMostViewed = () => {
-    return <></>;
+    return (
+      <>
+        <div className="flex flex-col items-center justify-start gap-4">
+          <TitleDivider label="پربازدید ترین مقالات" />
+          <div className="grid grid-cols-2 gap-2"></div>
+        </div>
+      </>
+    );
   };
 
   // for render latest posts
@@ -13,8 +38,8 @@ const ArticleList = () => {
 
   return (
     <div className="w-full grid grid-cols-12 gap-7">
-      <div className="col-span-9"></div>
-      <div className="col-span-3"></div>
+      <div className="col-span-9">{renderMostViewed()}</div>
+      <div className="col-span-3">{renderLatestPosts()}</div>
     </div>
   );
 };
