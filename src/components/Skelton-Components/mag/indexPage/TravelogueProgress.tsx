a55781ec7e48c436a34c1bddd607e0f60c9bbf78
danial <danialhooshangi41@gmail.com>
2025-03-15 18:53:50 +0330

    `Added Skeleton components to TravelogueProgress component`

--- src/components/Skelton-Components/mag/indexPage/TravelogueProgress.tsx
+++ src/components/Skelton-Components/mag/indexPage/TravelogueProgress.tsx
@@ -1,7 +1,21 @@
+import { Skeleton } from "@mui/material";
 import React from "react";
 
 const TravelogueProgress = () => {
-  return <div className="bg-orange-500 w-full">TravelogueProgress</div>;
+  return (
+    <div className="w-full flex items-center justify-start gap-3 overflow-x-auto">
+      {Array.from(Array(5)).map((item, index) => (
+        <Skeleton
+          key={index}
+          variant="rounded"
+          width={150}
+          height={150}
+          className="rounded-xl flex-shrink-0"
+          animation="wave"
+        />
+      ))}
+    </div>
+  );
 };
 
 export default TravelogueProgress;
