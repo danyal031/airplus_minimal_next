08b9d1e7310f51b655855940f8ca43a0b4a433a0
danial <danialhooshangi41@gmail.com>
2025-03-15 18:50:37 +0330

    `Added Skeleton components for travel news progress bar`

--- src/components/Skelton-Components/mag/indexPage/TravelNewsProgress.tsx
+++ src/components/Skelton-Components/mag/indexPage/TravelNewsProgress.tsx
@@ -1,7 +1,21 @@
+import { Skeleton } from "@mui/material";
 import React from "react";
 
 const TravelNewsProgress = () => {
-  return <div className="bg-green-400 w-full">TravelNewsProgress</div>;
+  return (
+    <div className="w-full flex items-center justify-start gap-3 overflow-x-auto">
+      {Array.from(Array(5)).map((item, index) => (
+        <Skeleton
+          key={index}
+          variant="rounded"
+          width={150}
+          height={150}
+          className="rounded-xl flex-shrink-0"
+          animation="wave"
+        />
+      ))}
+    </div>
+  );
 };
 
 export default TravelNewsProgress;
