c98a5228757ab6b4183d344b0e77557c908c45ae
danial <danialhooshangi41@gmail.com>
2025-04-10 13:24:01 +0330

    `Replace axios with customAxios in global-files/axioses.ts`

--- src/global-files/axioses.ts
+++ src/global-files/axioses.ts
@@ -13,7 +13,7 @@ import customAxios from "@/utils/customAxios";
 
 export const getAuthOtp = ({ mobile }: authInputType) => {
   return new Promise((resolve, reject) => {
-    axios
+    customAxios
       .post(
         ((process.env.NEXT_PUBLIC_BASE_URL_2 as string) +
           process.env.NEXT_PUBLIC_AUTH_OTP_ENDPOINT) as string,
@@ -34,7 +34,7 @@ export const getAuthOtp = ({ mobile }: authInputType) => {
 export const getAuthSubmit = ({ passenger_id, otp }: authSubmitInputType) => {
   console.log(passenger_id, otp);
   return new Promise((resolve, reject) => {
-    axios
+    customAxios
       .post(
         ((process.env.NEXT_PUBLIC_BASE_URL_2 as string) +
           process.env.NEXT_PUBLIC_AUTH_SUBMIT_ENDPOINT) as string,
@@ -59,7 +59,7 @@ export const getAuthSubmit = ({ passenger_id, otp }: authSubmitInputType) => {
 export const getCountryList = (props: baseDataDataType) => {
   console.log("sent data", props);
   return new Promise((resolve, reject) => {
-    axios
+    customAxios
       .get(process.env.NEXT_PUBLIC_BASE_URL_2 + "/base/data", {
         params: {
           action: props.action, // citizenship, countries, states, cities
@@ -101,10 +101,10 @@ export const handleFileUpload = async (e: any, type = "tmp", file = false) => {
 };
 
 export const handleStoreLotteryJson = async (data: any) => {
-  axios.defaults.headers.common["Authorization"] =
-    "Bearer " + localStorage.getItem("access_token");
+  // axios.defaults.headers.common["Authorization"] =
+  //   "Bearer " + localStorage.getItem("access_token");
   return new Promise((resolve, reject) => {
-    axios
+    customAxios
       .post(process.env.NEXT_PUBLIC_BASE_URL_2 + "/trade/store" + "", data)
       .then((response) => {
         // Handle successful response here
@@ -121,7 +121,7 @@ export const handleStoreLotteryJson = async (data: any) => {
 
 export const getPaymentStatus = async (refId: string) => {
   return new Promise((resolve, reject) => {
-    axios
+    customAxios
       .get(process.env.NEXT_PUBLIC_BASE_URL_2 + "/gateway/details", {
         params: { serial_id: refId },
       })
@@ -142,7 +142,7 @@ export const submitDiscountCode = async (data: any) => {
   console.log("discount: ", data);
 
   return new Promise((resolve, reject) => {
-    axios
+    customAxios
       .get(
         ((process.env.NEXT_PUBLIC_BASE_URL_2 as string) +
           process.env.NEXT_PUBLIC_SUBMIT_DISCOUNT_ENDPOINT) as string,
@@ -164,7 +164,7 @@ export const submitDiscountCode = async (data: any) => {
 };
 export const unSubmitDiscountCode = async (data: any) => {
   return new Promise((resolve, reject) => {
-    axios
+    customAxios
       .get(
         ((process.env.NEXT_PUBLIC_BASE_URL_2 as string) +
           process.env.NEXT_PUBLIC_UNSUBMIT_DISCOUNT_ENDPOINT) as string,
@@ -186,13 +186,13 @@ export const unSubmitDiscountCode = async (data: any) => {
 };
 
 export const getOrderList = async () => {
-  axios.defaults.headers.common["Authorization"] =
-    "Bearer " + localStorage.getItem("access_token");
+  // axios.defaults.headers.common["Authorization"] =
+  //   "Bearer " + localStorage.getItem("access_token");
   // axios.defaults.headers.common["Authorization"] =
   //   "Bearer " +
   //   "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0eXBlIjoiYjJjIiwiaXNzIjoiaHR0cDovLzE5Mi4xNjguMS4xMDAiLCJhdWQiOiJodHRwOi8vMTkyLjE2OC4xLjEwMCIsImlhdCI6MTcyODExMjM2NiwibmJmIjoxNzI4MTEyMzY2LCJleHAiOjE3Mjg3MTcxNjYsInV1aWQiOjkzLCJ1aXAiOiIxOTIuMTY4LjEuMTgiLCJicnciOnsidHlwZSI6ImxpYnJhcnkiLCJuYW1lIjoiUG9zdG1hbiBEZXNrdG9wIiwidmVyc2lvbiI6IjcuMzkuMSJ9fQ.5QKkhRTG2PvNCAuZf_eGzF6a5cm973aII51N4aITHjg";
   return new Promise((resolve, reject) => {
-    axios
+    customAxios
       .get(
         ((process.env.NEXT_PUBLIC_BASE_URL_2 as string) +
           process.env.NEXT_PUBLIC_SHOPPING_LIST_ENDPOINT) as string
@@ -212,13 +212,13 @@ export const getOrderList = async () => {
 
 // for Financial List request
 export const getFinancialList = async () => {
-  axios.defaults.headers.common["Authorization"] =
-    "Bearer " + localStorage.getItem("access_token");
+  // axios.defaults.headers.common["Authorization"] =
+  //   "Bearer " + localStorage.getItem("access_token");
   // axios.defaults.headers.common["Authorization"] =
   //   "Bearer " +
   //   "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0eXBlIjoiYjJjIiwiaXNzIjoiaHR0cDovLzE5Mi4xNjguMS4xMDAiLCJhdWQiOiJodHRwOi8vMTkyLjE2OC4xLjEwMCIsImlhdCI6MTcyODExMjM2NiwibmJmIjoxNzI4MTEyMzY2LCJleHAiOjE3Mjg3MTcxNjYsInV1aWQiOjkzLCJ1aXAiOiIxOTIuMTY4LjEuMTgiLCJicnciOnsidHlwZSI6ImxpYnJhcnkiLCJuYW1lIjoiUG9zdG1hbiBEZXNrdG9wIiwidmVyc2lvbiI6IjcuMzkuMSJ9fQ.5QKkhRTG2PvNCAuZf_eGzF6a5cm973aII51N4aITHjg";
   return new Promise((resolve, reject) => {
-    axios
+    customAxios
       .get(
         ((process.env.NEXT_PUBLIC_BASE_URL_2 as string) +
           process.env.NEXT_PUBLIC_FINANCIAL_LIST_ENDPOINT) as string
@@ -239,10 +239,10 @@ export const getFinancialList = async () => {
 // flight list
 export const getOnlineFlightSearch = (props: onlineFlightSearchInputType) => {
   console.log("props", props);
-  axios.defaults.headers.common["Domain"] = window.location.hostname;
+  // axios.defaults.headers.common["Domain"] = window.location.hostname;
 
   return new Promise((resolve, reject) => {
-    axios
+    customAxios
       .post(process.env.NEXT_PUBLIC_BASE_URL_2 + "/online/flight/search", props)
       .then((response) => {
         // Handle successful response here
@@ -260,7 +260,7 @@ export const getOnlineFlightSearch = (props: onlineFlightSearchInputType) => {
 // get airports list with axios without cache
 export const getAirports = () => {
   return new Promise((resolve, reject) => {
-    axios
+    customAxios
       .get(process.env.NEXT_PUBLIC_BASE_URL_2 + "/base/data", {
         params: {
           action: "airports",
@@ -282,10 +282,10 @@ export const getAirports = () => {
 
 // handle lock flight request
 export const lockFlight = (props: any) => {
-  axios.defaults.headers.common["Authorization"] =
-    "Bearer " + localStorage.getItem("access_token");
+  // axios.defaults.headers.common["Authorization"] =
+  //   "Bearer " + localStorage.getItem("access_token");
   return new Promise((resolve, reject) => {
-    axios
+    customAxios
       .post(
         (process.env.NEXT_PUBLIC_BASE_URL_2 as string) +
           process.env.NEXT_PUBLIC_LOCK_FLIGHT_ENDPOINT,
@@ -305,10 +305,10 @@ export const lockFlight = (props: any) => {
 };
 
 export const handleStoreFlightJson = (data: any) => {
-  axios.defaults.headers.common["Authorization"] =
-    "Bearer " + localStorage.getItem("access_token");
+  // axios.defaults.headers.common["Authorization"] =
+  //   "Bearer " + localStorage.getItem("access_token");
   return new Promise((resolve, reject) => {
-    axios
+    customAxios
       .post(process.env.NEXT_PUBLIC_BASE_URL_2 + "/trade/store", data)
       .then((response) => {
         // Handle successful response here
@@ -327,7 +327,7 @@ export const handleStoreFlightJson = (data: any) => {
 
 export const recommendedAccommodations = (data: string) => {
   return new Promise((resolve, reject) => {
-    axios
+    customAxios
       .get(
         (process.env.NEXT_PUBLIC_BASE_URL_2 as string) +
           process.env.NEXT_PUBLIC_ACCOMMODATIONS_ENDPOINT,
@@ -359,10 +359,10 @@ export const getAccommodationsList = (
   child_capacity: number,
   page: number
 ) => {
-  axios.defaults.headers.common["Domain"] = window.location.hostname;
+  // axios.defaults.headers.common["Domain"] = window.location.hostname;
 
   return new Promise((resolve, reject) => {
-    axios
+    customAxios
       .get(
         ((process.env.NEXT_PUBLIC_BASE_URL_2 as string) +
           process.env.NEXT_PUBLIC_ACCOMMODATIONS_LIST_ENDPOINT) as string,
@@ -397,7 +397,7 @@ export const getMinPrice = (
   accommodationId: number[]
 ) => {
   return new Promise((resolve, reject) => {
-    axios
+    customAxios
       .get(
         ((process.env.NEXT_PUBLIC_BASE_URL_2 as string) +
           process.env
@@ -429,7 +429,7 @@ export const getRoomTypesAccommodation = (
   checkout_date: string
 ) => {
   return new Promise((resolve, reject) => {
-    axios
+    customAxios
       .get(
         ((process.env.NEXT_PUBLIC_BASE_URL_2 as string) +
           process.env
@@ -460,10 +460,10 @@ export const getAccommodationDetails = (
   checkin_date: string,
   checkout_date: string
 ) => {
-  axios.defaults.headers.common["Domain"] = window.location.hostname;
+  // axios.defaults.headers.common["Domain"] = window.location.hostname;
 
   return new Promise((resolve, reject) => {
-    axios
+    customAxios
       .get(
         ((process.env.NEXT_PUBLIC_BASE_URL_2 as string) +
           process.env.NEXT_PUBLIC_GET_ACCOMMODATION_DETAILS) as string,
@@ -490,11 +490,11 @@ export const getAccommodationDetails = (
 
 // handle lock residence
 export const lockResidence = (props: any) => {
-  axios.defaults.headers.common["Authorization"] =
-    "Bearer " + localStorage.getItem("access_token");
+  // axios.defaults.headers.common["Authorization"] =
+  //   "Bearer " + localStorage.getItem("access_token");
 
   return new Promise((resolve, reject) => {
-    axios
+    customAxios
       .post(
         (process.env.NEXT_PUBLIC_BASE_URL_2 as string) +
           process.env.NEXT_PUBLIC_LOCK_RESIDENCE_ENDPOINT,
