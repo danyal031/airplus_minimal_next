1518d8760c8d935f59c65ceb9e70df32ed8b0325
Danial <danialhooshangi41@gmail.com>
2025-06-06 12:14:50 +0330

    `Removed generateMetadata function and commented out code in layout.tsx and updated App.tsx with minor changes`

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,54 +4,57 @@ import { GlobalContextProvider } from "@/context/store";
 import { iranyekanxFonts } from "./theme/localFont";
 import { headers } from "next/headers";
 
-export async function generateMetadata() {
-  const host = headers().get("host");
+// export async function generateMetadata() {
+//   const host = headers().get("host");
 
-  const res = await fetch(process.env.NEXT_PUBLIC_BASE_URL_2 + "/config", {
-    cache: "no-store",
-    headers: {
-      // domain: "localhost",
-      domain: host as string,
-    },
-    // next: {
-    //   tags: ["config"],
-    // },
-  });
-  // if (!res.ok) throw new Error("Failed to fetch config");
-  const data = await res.json();
-  // console.log(data);
+//   const res = await fetch(process.env.NEXT_PUBLIC_BASE_URL_2 + "/config", {
+//     cache: "no-store",
+//     headers: {
+//       // domain: "localhost",
+//       domain: host as string,
+//     },
+//     // next: {
+//     //   tags: ["config"],
+//     // },
+//   });
+//   // if (!res.ok) throw new Error("Failed to fetch config");
+//   const data = await res.json();
+//   // console.log(data);
 
-  return {
-    title: {
-      template: `%s | ${data.brand.fa}`,
-      default: "خرید بلیت هواپیما",
-    },
-    description: data.hub.description,
-    other: {
-      enamad: data.hub.authentication,
-    },
-    icons: {
-      icon: `${process.env.NEXT_PUBLIC_MEDIA_URL_1}/media/branches/favicon/${data.design.favicon}`,
-      // shortcut: "icon",
-    },
-  };
-}
+//   return {
+//     title: {
+//       template: `%s | ${data.brand.fa}`,
+//       default: "خرید بلیت هواپیما",
+//     },
+//     description: data.hub.description,
+//     other: {
+//       enamad: data.hub.authentication,
+//     },
+//     icons: {
+//       icon: `${process.env.NEXT_PUBLIC_MEDIA_URL_1}/media/branches/favicon/${data.design.favicon}`,
+//       // shortcut: "icon",
+//     },
+//   };
+// }
 
 // export const metaData = {};
 
-export default function RootLayout({
+
+
+export default function RootLayout ( {
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) {
+}> )
+{
   return (
     <html
       lang="en"
       dir="rtl"
-      className={`${iranyekanxFonts.variable} font-sans ss02`}
+      className={ `${ iranyekanxFonts.variable } font-sans ss02` }
     >
-      <body className={`${iranyekanxFonts.variable} font-sans ss02`}>
-        <GlobalContextProvider>{children}</GlobalContextProvider>
+      <body className={ `${ iranyekanxFonts.variable } font-sans ss02` }>
+        <GlobalContextProvider>{ children }</GlobalContextProvider>
       </body>
     </html>
   );
